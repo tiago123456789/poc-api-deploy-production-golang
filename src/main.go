ae6df896370cba90aced7cfe14ef90ce6b491076
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-06-23 17:52:27 -0300

    changes

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -40,6 +41,6 @@ func main() {
 		return c.SendString(msg) // => ✋ register
 	})
 
-	log.Fatal(app.Listen(":"))
-
+	port := os.Getenv(("PORT"))
+	log.Fatal(app.Listen(":" + port))
 }
