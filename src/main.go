2a8d46998e3262e3897b3e06fd86bd90b22c36bb
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-06-29 16:41:04 -0300

    alterações para fazer deploy aplicação em docker running in heroku

--- src/main.go
+++ src/main.go
@@ -6,14 +6,13 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	app := fiber.New()
 
